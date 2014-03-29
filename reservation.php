096d539f8f3e4fb83b8e84dd32984a18085ec8ed
Schmoopy42 <jerryboy1993@gmail.com>
2014-03-29 19:58:29 -0400

    Added enddate

--- reservation.php
+++ reservation.php
@@ -33,7 +33,7 @@
 
 			<br><br> Select a start date for your reservation:
 
-			<select name = 'month'>
+			<select name = 'startmonth'>
 				<option value="01">January</option>
 				<option value="02">Febrary</option>
 				<option value="03">March</option>
@@ -48,7 +48,41 @@
 				<option value="011">December</option>
 			</select>
 
-			<select name = 'Year'>
+			<select name = "startday">
+                <option value = "01">1</option>
+                <option value = "02">2</option>
+                <option value = "03">3</option>
+                <option value = "04">4</option>
+                <option value = "05">5</option>
+                <option value = "06">6</option>
+                <option value = "07">7</option>
+                <option value = "08">8</option>
+                <option value = "09">9</option>
+                <option value = "10">10</option>
+                <option value = "11">11</option>
+                <option value = "12">12</option>
+                <option value = "13">13</option>
+                <option value = "14">14</option>
+                <option value = "15">15</option>
+                <option value = "16">16</option>
+                <option value = "17">17</option>
+                <option value = "18">18</option>
+                <option value = "19">19</option>
+                <option value = "20">20</option>
+                <option value = "21">21</option>
+                <option value = "22">22</option>
+                <option value = "23">23</option>
+                <option value = "24">24</option>
+                <option value = "25">25</option>
+                <option value = "26">26</option>
+                <option value = "27">27</option>
+                <option value = "28">28</option>
+                <option value = "29">29</option>
+                <option value = "30">30</option>
+                <option value = "31">31</option>
+            </select>
+
+			<select name = 'startyear'>
 				<option value="2014">2014</option>
 				<option value="2015">2015</option>
 			</select>
@@ -156,6 +190,60 @@
 			</select>
 
 			<br><br> Select an end date for your reservation:
+
+			<select name = 'endmonth'>
+				<option value="01">January</option>
+				<option value="02">Febrary</option>
+				<option value="03">March</option>
+				<option value="04">April</option>
+				<option value="04">May</option>
+				<option value="05">June</option>
+				<option value="06">July</option>
+				<option value="07">August</option>
+				<option value="08">September</option>
+				<option value="09">October</option>
+				<option value="010">November</option>
+				<option value="011">December</option>
+			</select>
+
+			<select name = "endday">
+                <option value = "01">1</option>
+                <option value = "02">2</option>
+                <option value = "03">3</option>
+                <option value = "04">4</option>
+                <option value = "05">5</option>
+                <option value = "06">6</option>
+                <option value = "07">7</option>
+                <option value = "08">8</option>
+                <option value = "09">9</option>
+                <option value = "10">10</option>
+                <option value = "11">11</option>
+                <option value = "12">12</option>
+                <option value = "13">13</option>
+                <option value = "14">14</option>
+                <option value = "15">15</option>
+                <option value = "16">16</option>
+                <option value = "17">17</option>
+                <option value = "18">18</option>
+                <option value = "19">19</option>
+                <option value = "20">20</option>
+                <option value = "21">21</option>
+                <option value = "22">22</option>
+                <option value = "23">23</option>
+                <option value = "24">24</option>
+                <option value = "25">25</option>
+                <option value = "26">26</option>
+                <option value = "27">27</option>
+                <option value = "28">28</option>
+                <option value = "29">29</option>
+                <option value = "30">30</option>
+                <option value = "31">31</option>
+            </select>
+
+			<select name = 'endyear'>
+				<option value="2014">2014</option>
+				<option value="2015">2015</option>
+			</select>
 
 			<br><br> Select an end time for your reservation:
 
