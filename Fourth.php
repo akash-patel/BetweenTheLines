f37f4c594fc315a3f1c9c69681686d8f1380a72b
mihailstantchev <mihail.stantchev@gmail.com>
2014-03-31 00:21:53 -0400

    Goes to photosensor simulation
    Fourth now goes to photosensor simulation

--- Fourth.php
+++ Fourth.php
@@ -2,17 +2,20 @@
 <div align="center">
 <h1>Welcome to the Elevator</h1>
 <P> Please wait while we take you to the next available floor.</P>
-
-
+<?php 
+	session_start();
+?>
+<br>
 <script type="text/javascript">
 
 (function () {
     var currentfloor = 1,cinterval;
+	var a = <?=$_SESSION['floor']?>;
 
     var timeDec = function (){
         currentfloor++;
         document.getElementById('countdown').innerHTML = currentfloor;
-        if(currentfloor === 5){
+        if(currentfloor === a){
             clearInterval(cinterval);
         }
     };
@@ -24,14 +27,20 @@
 
 Current floor <span id="countdown">1</span>.
 <br>
-<input name="continue" id="continue" type="button" value="Exit Elevator" onclick="window.open('Third.php', '_parent')"/>
+<input name="continue" id="continue" type="button" value="Exit Elevator" onclick="window.open('photosensor.php', '_parent')"/>
 <script type="text/javascript">
+var a = <?=$_SESSION['floor']?>;
+var b = a*1000-1000;
 document.getElementById('continue').disabled = true;
 document.getElementById('continue').value = "Please wait...";
-setTimeout(function(){
-  document.getElementById('continue').disabled = false;
-  document.getElementById('continue').value = "Continue";
-}, 4000);
+setTimeout
+
+(function(){
+	
+	
+	document.getElementById('continue').disabled = false;
+	document.getElementById('continue').value = "Continue";
+}, b);
 </script>
 </div>
 
