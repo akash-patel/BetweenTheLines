7eca961711f074bc938f128f474bb73603c54403
mihailstantchev <mihail.stantchev@gmail.com>
2014-03-31 00:21:12 -0400

    Updates occupancy
    This updates the database based on what spots are clicked on the
    photosensor page.

---
+++ updateoccupancy.php
@@ -0,0 +1,23 @@
+<?php
+	session_start();
+		include('config.inc'); 
+				$sid=$_SESSION['floor']*100+$_POST['spot'];
+				$query2 = "SELECT * FROM occupancy WHERE psensorid ='$sid'";
+				$result2 = mysql_query($query2);
+				$row = mysql_fetch_array($result2);
+				if ($row['istaken']==0)
+				{
+				$query = "UPDATE occupancy SET istaken='1' WHERE psensorid='$sid'";
+				$result = mysql_query($query);
+				}
+				else
+				{
+				$query = "UPDATE occupancy SET istaken='0' WHERE psensorid='$sid'";
+				$result = mysql_query($query);
+				}
+				
+
+				$_SESSION['update'] = "Database has been updated";
+				header('Location: photosensor.php');
+
+?>
