6cb13d7e4ce9d11b977b395b419625219b644dc1
Akash Patel <abp102@scarletmail.rutgers.edu>
2014-05-04 19:33:02 -0400

    Added logos/favicons to use

--- header.php
+++ header.php
@@ -1,4 +1,7 @@
 <!DOCTYPE html>
 <html>
+	<link rel="icon" href="BTL.gif">
+	<a href="securedpage.php"><img src="BetweenTheLines.gif"></a>
+	<hr>
 	<body style= "background-color: #F0F0F0 "></body>
 </html>
